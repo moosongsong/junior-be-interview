3563b2051961055d5cc360c373c080688ce8aaa2
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-05-11 01:37:11 +0900

    :tada: chore: 기본 vuepress 템플릿 설정

---
+++ .vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    title: '주니어 백엔드 개발자 인터뷰',
+    description: '지금까지 지원한 기업들의 지원서를 몰아보자',
+    base: "/junior-be-interview/",
+    dest: 'build',
+    themeConfig: {
+        logo: 'https://avatars.githubusercontent.com/u/50647845?v=4',
+        nav: [
+            {text: 'Home', link: '/'}
+        ],
+        sidebar: [
+            {
+                title: '디자인패턴',
+                collapsable: true,
+                children: [
+                    '/01_디자인패턴/01_싱글톤',
+                    '/01_디자인패턴/02_팩토리메소드',
+                    '/01_디자인패턴/03_추상팩토리',
+                    '/01_디자인패턴/04_빌더',
+                    '/01_디자인패턴/05_프로토타입',
+                    '/01_디자인패턴/06_어댑터',
+                    '/01_디자인패턴/07_브릿지',
+                ]
+            }
+        ]
+    }
+}
